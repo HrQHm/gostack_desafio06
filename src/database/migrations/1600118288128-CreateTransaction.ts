214933e0716c2da55f605fe34a9704cc00b62e10
Henrique Makita <henrique_hmk@hotmail.com>
2020-09-17 19:13:27 -0300

    Finalizando Exercício

---
+++ src/database/migrations/1600118288128-CreateTransaction.ts
@@ -0,0 +1,56 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateTransaction1600118288128
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'transactions',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+          },
+          {
+            name: 'type',
+            type: 'varchar',
+          },
+          {
+            name: 'value',
+            type: 'decimal',
+            precision: 10,
+            scale: 2,
+          },
+          {
+            name: 'category_id',
+            type: 'uuid',
+            isNullable: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+            isNullable: false,
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('transactions');
+  }
+}
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,55 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Error from '../errors/AppError';
+
+interface Request {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    type,
+    value,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getRepository(Category);
+
+    let categoryTransaction = await categoryRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!categoryTransaction) {
+      categoryTransaction = categoryRepository.create({
+        title: category,
+      });
+
+      await categoryRepository.save(categoryTransaction);
+    }
+
+    if (type === 'outcome') {
+      const total = transactionRepository.getBalance();
+      if (value > (await total).total) {
+        throw new Error('Output greater than income', 400);
+      }
+    }
+
+    const transaction = transactionRepository.create({
+      title,
+      type,
+      value,
+      category: categoryTransaction,
+    });
+
+    await transactionRepository.save(transaction);
+    return transaction;
   }
 }
 
